ee440fa3e29277107e2090ea445b5b911dbdb4d2
Shweta Sampatrao Kale <Shweta.Kale@asl.mum.sst>
2024-07-20 08:48:54 +0530

    Reflector Assignment

---
+++ Account.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflector
+{
+    internal class Account
+    {
+
+        private int _accountNumber;
+        private string _name;
+        private double _balance;
+
+        public int AccountNumber { get; set; }
+        public string Name { get; set; }
+
+        static double Min_Balance = 500;
+
+        public double Balance { get; set; } = Min_Balance;
+
+        public string AadharNum { get; set; } //property
+
+        //constructors
+
+        public Account(int _accountNumber, string _name)
+        {
+            AccountNumber = _accountNumber;
+            Name = _name;
+        }
+
+        public Account(int _accountNumber, string _name, double _balance) : this(_accountNumber, _name)
+        {
+            Balance = Min_Balance;
+        }
+
+        public Account(int _accountNumber, string _name, double _balance, string _aadharNum) : this(_accountNumber, _name, _balance)
+        {
+            AadharNum = _aadharNum;
+        }
+
+        public Account()
+        {
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Reflection;
+
+namespace Reflector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly executing = Assembly.GetExecutingAssembly();
+
+            Type[] types = executing.GetTypes();
+
+            foreach (var item in types)
+            {
+                Console.WriteLine("Class : {0}", item.Name);
+
+                // Count methods
+                MethodInfo[] methods = item.GetMethods();
+                int methodCount = methods.Length;
+                Console.WriteLine("Method Count: {0}", methodCount);
+
+                // Print methods and their parameters (if any)
+                if (methodCount > 0)
+                {
+                    foreach (var method in methods)
+                    {
+                        // Count parameters
+                        ParameterInfo[] parameters = method.GetParameters();
+                        int parameterCount = parameters.Length;
+                        Console.WriteLine("--> Method: {0}", method.Name);
+                        Console.WriteLine("    Parameter Count: {0}", parameterCount);
+
+                        // Print each parameter
+                        foreach (var param in parameters)
+                        {
+                            Console.WriteLine("    Parameter: {0} - Type: {1}", param.Name, param.ParameterType);
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No methods.");
+                }
+
+                // Count constructors
+                ConstructorInfo[] ctors = item.GetConstructors();
+                int constructorCount = ctors.Length;
+                Console.WriteLine("Constructor Count: {0}", constructorCount);
+
+                // Print constructors and their parameters (if any)
+                if (constructorCount > 0)
+                {
+                    foreach (var ctor in ctors)
+                    {
+                        // Count parameters
+                        ParameterInfo[] parameters = ctor.GetParameters();
+                        int parameterCount = parameters.Length;
+                        Console.WriteLine("--> Constructor: {0}", ctor);
+                        Console.WriteLine("    Parameter Count: {0}", parameterCount);
+
+                        // Print each parameter
+                        foreach (var param in parameters)
+                        {
+                            Console.WriteLine("    Parameter: {0} - Type: {1}", param.Name, param.ParameterType);
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No constructors.");
+                }
+
+                Console.WriteLine(); // Separate classes with an empty line
+            }
+
+            Account accountInstance = new Account();
+            Program programInstance = new Program();
+            ShowDetails(accountInstance, programInstance);
+        }
+
+        static void ShowDetails(object obj1, object obj2)
+        {
+            Type type1 = obj1.GetType();
+            Type type2 = obj2.GetType();
+
+            Console.WriteLine("Details for {0}:", type1.Name);
+            Console.WriteLine("Full Name: {0}", type1.FullName);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Details for {0}:", type2.Name);
+            Console.WriteLine("Full Name: {0}", type2.FullName);
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
